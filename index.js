86a221f5bcd16e735ce8f18c61ce19275ce37e85
Xcution <xcution.js@gmail.com>
2024-10-15 02:03:59 +0700

    start manager

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('file-manager')
+import { createInterface } from 'readline';
+import { homedir } from 'os';
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let currentDir = homedir();
+let username;
+
+const args = process.argv.slice(2);
+const usernameArg = args.find(arg => arg.startsWith('--username='));
+username = usernameArg ? usernameArg.split('=')[1] : 'User';
+
+console.log(`Welcome to the File Manager, ${username}!`);
+console.log(`You are currently in ${currentDir}`);
+
+rl.on('close', () => {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    process.exit(0);
+});
